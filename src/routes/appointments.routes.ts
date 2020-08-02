159c6ed3731aa2b6f55cf403ce812bd9e5549551
Jefferson da Silva <je_jss@hotmail.com>
2020-08-02 01:58:54 -0300

    update appointment route

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, request } from 'express';
 import { startOfHour, parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
@@ -6,23 +6,30 @@ import AppointmentsRepository from '../repositories/AppointmentsRepository';
 const appointmentRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentRouter.get('/', (request, response) => {
+    const appointments = appointmentsRepository.allAppointments();
+
+    return response.json(appointments);
+});
 
 appointmentRouter.post('/', (request, response) => {
     const { provider, date } = request.body;
 
     const parseDate = startOfHour(parseISO(date));
 
-    const findAppointmentSameDate = appointmentsRepository.findByDate(parseDate);
+    const findAppointmentSameDate = appointmentsRepository.findByDate(
+        parseDate,
+    );
 
     if (findAppointmentSameDate) {
-        return response.status(400).json({ message: 'This appointment is not avaible' })
+        return response
+            .status(400)
+            .json({ message: 'This appointment is not avaible' });
     }
 
     const appointment = appointmentsRepository.create(provider, parseDate);
 
-
     return response.json(appointment);
-})
-
+});
 
 export default appointmentRouter;
